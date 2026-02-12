dbeb0c4e68051dfa1bf5939232f04297caf98fe7
kjh <kjh4940@naver.com>
2026-02-12 18:21:08 +0900

    chore: Spring Boot 프로젝트 초기화

---
+++ src/test/java/com/punch/shop/PunchShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punch.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
