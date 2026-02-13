c1e7242af2ddc8f8c2de3c60f323d15ba881c13c
kjh <kjh4940@naver.com>
2026-02-13 16:33:51 +0900

    feat: 홈페이지 기본 템플릿 작성
    - index.html 생성 (히어로 섹션, 카테고리, 추천 상품, 혜택 안내)
    - HomeController 생성 (루트 경로 매핑)

---
+++ src/main/java/com/punch/shop/common/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.punch.shop.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
