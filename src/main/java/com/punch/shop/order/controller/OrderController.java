23e6006a9ab684d2fbc168d8835e84a8174e60c2
kjh <kjh4940@naver.com>
2026-03-27 19:04:37 +0900

    fix: 주문내역 최신순 정렬

--- src/main/java/com/punch/shop/order/controller/OrderController.java
+++ src/main/java/com/punch/shop/order/controller/OrderController.java
@@ -16,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -105,7 +106,7 @@ public class OrderController {
 
     @GetMapping
     public String orderList(@AuthenticationPrincipal MemberPrincipal principal,
-                            @PageableDefault(size = 10) Pageable pageable,
+                            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
                             Model model) {
         Page<OrderListItemResponse> orders = orderService.getOrders(principal.getMemberId(), pageable);
         model.addAttribute("orders", orders);
