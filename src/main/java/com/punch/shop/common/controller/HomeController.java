039045e07043f10bd56f4ba9aa0571f3260de636
kjh <kjh4940@naver.com>
2026-03-28 15:10:53 +0900

    feat: 메인 페이지 카테고리 버튼 상품 페이지 연결 및 카테고리 시드 데이터 추가

--- src/main/java/com/punch/shop/common/controller/HomeController.java
+++ src/main/java/com/punch/shop/common/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.punch.shop.common.controller;
 
+import com.punch.shop.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final ProductService productService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("categories", productService.getCategories());
         return "index";
     }
 }
