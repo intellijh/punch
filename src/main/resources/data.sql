8329626912b18e93c0db9bb3a5934d5daae15202
kjh <kjh4940@naver.com>
2026-04-25 00:11:19 +0900

    chore: 데이터 초기화 파일 제거

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT IGNORE INTO category (name, created_at, updated_at) VALUES
-    ('가전/디지털', NOW(), NOW()),
-    ('패션/의류',   NOW(), NOW()),
-    ('식품',       NOW(), NOW()),
-    ('뷰티',       NOW(), NOW()),
-    ('스포츠/레저', NOW(), NOW());
